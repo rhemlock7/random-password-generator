058a6515cf7c66306ba2e7a13df669a27d7fbf29
Ryan Hemlock <ryanhemlock@Ashleys-MacBook-Pro.local>
2023-11-22 06:56:55 -0500

    Created alert prompts

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 // Assignment code here
 
-
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+
+
+function generatePassword() {
+    //Alert prompts
+    let characters = prompt('How many characters should your password include?');
+    if (characters < 8 || characters > 128) {
+      confirm('Character limit is between 8 and 128. Please try again.');
+      let characters = prompt('How many characters should your password include?');
+    }
+    console.log(characters);
+
+    let lowerCase = confirm("Do you want lowercase letters included?");
+    console.log(lowerCase);
+
+    let upperCase = confirm("Do you want upperCase letters included?");
+    console.log(upperCase);
+
+    let numbers = confirm("Do you want numbers included?");
+    console.log(numbers);
+
+    let specialCharacters = confirm("Do you want special characters included? (<>!!@#$!%)");
+    console.log(specialCharacters);
+
+    //Logic to create the password
+    for (i=0; i < characters; i++) {
+      
+    }
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -15,3 +42,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//Create alerts onClick = Complete
+
+/** Prompts:
+ * How many characters should your password include?
+ * Do you want lowercase letters included?
+ * Do you want UPPERCASE letters included?
+ * Do you want numbers?
+ * Do you want special characters? (<>?!@#$)?
+ */
+
+//Take various inputs = Complete
+
+//Create logic based on inputs
+
+//Generate password when logic has been confirmed
