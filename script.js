6e3dffdd4d9f955134651e583af91dbd138b895c
Ryan Hemlock <ryan@ryanhemlock.com>
2023-11-22 09:56:52 -0500

    Changed alert inputs to concat characters to an empty string

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
     //Characters to include within the password
-    let characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&()*+,-./:;<=>?@[^_`{|}~';
+    // let characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&()*+,-./:;<=>?@[^_`{|}~';
+    let characters ='';
 
     //Alert prompts
     let numberOfCharacters = prompt('How many characters should your password include?');
@@ -17,26 +18,23 @@ function generatePassword() {
     console.log(numberOfCharacters);
 
     let lowerCase = confirm("Do you want lowercase letters included?");
-    if (lowerCase === false) {
-      let pattern = new RegExp("[a-z]",'g');
-      characters = characters.replace(pattern, "");
+    if (lowerCase === true) {
+      characters = characters.concat('abcdefghijklmnopqrstuvwxyz');
     }
 
     console.log(characters)
     console.log(lowerCase);
 
     let upperCase = confirm("Do you want upperCase letters included?");
-    if (upperCase === false) {
-      let pattern = new RegExp("[A-Z]",'g');
-      characters = characters.replace(pattern, "");
+    if (upperCase === true) {
+      characters = characters.concat('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     }
     console.log(characters)
     console.log(upperCase);
 
     let numbers = confirm("Do you want numbers included?");
-    if (numbers === false) {
-      let pattern = new RegExp("[0-9]",'g');
-      characters = characters.replace(pattern, "");
+    if (numbers === true) {
+      characters = characters.concat('0123456789');
     }
 
     console.log(characters);
@@ -44,9 +42,8 @@ function generatePassword() {
 
     //Need to fix special characters deleting the entire string
     let specialCharacters = confirm("Do you want special characters included? (<>!!@#$!%)");
-    if (specialCharacters === false) {
-      let pattern = new RegExp("[!-~]",'g');
-      characters = characters.replace(pattern, "");
+    if (specialCharacters === true) {
+      characters = characters.concat('!"#$%&()*+,-./:;<=>?@[^_`{|}~');
     }
 
     console.log(characters);
@@ -77,25 +74,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-//Create alerts onClick = Complete
-
-/** Prompts:
- * How many characters should your password include?
- * Do you want lowercase letters included?
- * Do you want UPPERCASE letters included?
- * Do you want numbers?
- * Do you want special characters? (<>?!@#$)?
- */
-
-//Take various inputs = Complete
-
-//Create logic based on inputs
-  //Need a for loop to randomize each character of the password.
-  //Specify the parameters for each character's randomization.
-
-//Generate password when logic has been confirmed = COMPLETE
 
 
 //Learned RegExp from GeeksForGeeks:
